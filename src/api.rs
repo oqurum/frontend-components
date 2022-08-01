f75a045e56b5f4576649ece0a57e8d3d3ab4f8b7
Its <2779546+Its-its@users.noreply.github.com>
2022-08-01 13:54:27 -0700

    Tabs -> Spaces

--- src/api.rs
+++ src/api.rs
@@ -3,16 +3,16 @@ use serde::{Serialize, Deserialize};
 // List Response
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct QueryListResponse<V> {
-	pub offset: usize,
-	pub limit: usize,
-	pub total: usize,
-	pub items: Vec<V>,
+    pub offset: usize,
+    pub limit: usize,
+    pub total: usize,
+    pub items: Vec<V>,
 }
 
 // Deletion Response
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct DeletionResponse {
-	pub total: usize,
+    pub total: usize,
 }
 
 
@@ -20,57 +20,57 @@ pub struct DeletionResponse {
 // TODO: Could just be an enum.
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct WrappingResponse<V> {
-	pub resp: Option<V>,
-	pub error: Option<ApiErrorResponse>,
+    pub resp: Option<V>,
+    pub error: Option<ApiErrorResponse>,
 }
 
 impl<V> WrappingResponse<V> {
-	pub fn okay(value: V) -> Self {
-		Self { resp: Some(value), error: None }
-	}
+    pub fn okay(value: V) -> Self {
+        Self { resp: Some(value), error: None }
+    }
 
-	pub fn error<S: Into<String>>(value: S) -> Self {
-		Self { resp: None, error: Some(ApiErrorResponse::new(value)) }
-	}
+    pub fn error<S: Into<String>>(value: S) -> Self {
+        Self { resp: None, error: Some(ApiErrorResponse::new(value)) }
+    }
 
-	pub fn ok(self) -> std::result::Result<V, ApiErrorResponse> {
-		if let Some(resp) = self.resp {
-			Ok(resp)
-		} else if let Some(err) = self.error {
-			Err(err)
-		} else {
-			unreachable!()
-		}
-	}
+    pub fn ok(self) -> std::result::Result<V, ApiErrorResponse> {
+        if let Some(resp) = self.resp {
+            Ok(resp)
+        } else if let Some(err) = self.error {
+            Err(err)
+        } else {
+            unreachable!()
+        }
+    }
 
-	pub fn as_ok(&self) -> std::result::Result<&V, &ApiErrorResponse> {
-		if let Some(resp) = self.resp.as_ref() {
-			Ok(resp)
-		} else if let Some(err) = self.error.as_ref() {
-			Err(err)
-		} else {
-			unreachable!()
-		}
-	}
+    pub fn as_ok(&self) -> std::result::Result<&V, &ApiErrorResponse> {
+        if let Some(resp) = self.resp.as_ref() {
+            Ok(resp)
+        } else if let Some(err) = self.error.as_ref() {
+            Err(err)
+        } else {
+            unreachable!()
+        }
+    }
 
-	pub fn map<N, F: Fn(V) -> N>(self, func: F) -> WrappingResponse<N> {
-		WrappingResponse {
-			resp: self.resp.map(func),
-			error: self.error,
-		}
-	}
+    pub fn map<N, F: Fn(V) -> N>(self, func: F) -> WrappingResponse<N> {
+        WrappingResponse {
+            resp: self.resp.map(func),
+            error: self.error,
+        }
+    }
 }
 
 
 #[derive(Debug, Serialize, Deserialize, Clone, thiserror::Error)]
 pub struct ApiErrorResponse {
-	pub description: String,
+    pub description: String,
 }
 
 impl ApiErrorResponse {
-	pub fn new<S: Into<String>>(value: S) -> Self {
-		Self { description: value.into() }
-	}
+    pub fn new<S: Into<String>>(value: S) -> Self {
+        Self { description: value.into() }
+    }
 }
 
 impl std::fmt::Display for ApiErrorResponse {
