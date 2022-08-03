3201281bc49baf9c96bf7cd365a7549e74b631ee
Its <2779546+Its-its@users.noreply.github.com>
2022-08-03 15:56:44 -0700

    Webpage: Add Navbar

--- examples/webpage/src/app.rs
+++ examples/webpage/src/app.rs
@@ -7,7 +7,13 @@ use crate::routes::{switch, AppRoute};
 pub fn app() -> Html {
     html! {
         <BrowserRouter>
-            // TODO: Navbar
+            <div class="navbar-module">
+                <div class="left-content">
+                    <Link<AppRoute> to={ AppRoute::Home }>{ "Home" }</Link<AppRoute>>
+                </div>
+                <div class="center-content"></div>
+                <div class="right-content"></div>
+            </div>
             <div class="outer-view-container">
                 <div class="view-container">
                     <Switch<AppRoute> render={ Switch::render(switch) } />
