0ae75b7fe3f7c7ff3b7b7fea1966ded067e1d568
Its <2779546+Its-its@users.noreply.github.com>
2022-08-06 22:16:29 -0700

    ThumbnailStore changes

--- src/specific/image.rs
+++ src/specific/image.rs
@@ -42,41 +42,18 @@ impl ThumbnailStore {
         !self.is_none()
     }
 
-    pub fn as_url(&self) -> String {
+    pub fn as_value(&self) -> Option<&str> {
         match self {
-            Self::None => String::from(MISSING_THUMB_PATH),
-            _ => format!("/api/v1/image/{}", self.as_value()),
+            Self::Path(v) => Some(v.as_str()),
+            Self::None => None,
         }
     }
 
-
-    pub fn as_value(&self) -> &str {
+    pub fn into_value(self) -> Option<String> {
         match self {
-            Self::Path(v) => v.as_str(),
-            _ => unreachable!("Self::as_value()"),
+            Self::Path(v) => Some(v),
+            Self::None => None,
         }
-    }
-
-    pub fn into_value(self) -> String {
-        match self {
-            Self::Path(v) => v,
-            _ => unreachable!("Self::into_value()"),
-        }
-    }
-
-    pub fn to_optional_string(&self) -> Option<String> {
-        if self.is_some() {
-            Some(self.to_string())
-        } else {
-            None
-        }
-    }
-}
-
-
-impl ToString for ThumbnailStore {
-    fn to_string(&self) -> String {
-        self.as_value().to_string()
     }
 }
 
@@ -118,10 +95,6 @@ impl Serialize for ThumbnailStore {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer {
-        if self.is_some() {
-            serializer.serialize_str(&self.to_string())
-        } else {
-            serializer.serialize_none()
-        }
+        self.as_value().serialize(serializer)
     }
 }
