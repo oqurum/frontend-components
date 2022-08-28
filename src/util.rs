b5461a925c594f421da14f70ac3bf40803d5e10d
Its <2779546+Its-its@users.noreply.github.com>
2022-08-27 21:19:01 -0700

    Impl serialize/deserialize fns for DateTime & Date Add Librarian Search Query and Results

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,6 @@
+use chrono::{Date, Utc, DateTime, TimeZone};
+use serde::{Serializer, Deserializer, Serialize, Deserialize};
+
 /// Truncate string based off of char indices instead of bytes.
 pub fn truncate_on_indices(s: &mut String, max_chars: usize) {
     if let Some((new_len, _)) = s.char_indices().nth(max_chars) {
@@ -50,4 +53,53 @@ mod frontend {
             false
         }
     }
+}
+
+
+// DateTime
+
+pub fn serialize_datetime<S>(value: &DateTime<Utc>, s: S) -> std::result::Result<S::Ok, S::Error> where S: Serializer {
+    s.serialize_i64(value.timestamp_millis())
+}
+
+pub fn serialize_datetime_opt<S>(value: &Option<DateTime<Utc>>, s: S) -> std::result::Result<S::Ok, S::Error> where S: Serializer {
+    match value {
+        Some(v) => s.serialize_i64(v.timestamp_millis()),
+        None => s.serialize_none()
+    }
+}
+
+pub fn serialize_datetime_opt_opt<S>(value: &Option<Option<DateTime<Utc>>>, s: S) -> std::result::Result<S::Ok, S::Error> where S: Serializer {
+    value.map(|v| v.map(|v| v.timestamp_millis())).serialize(s)
+}
+
+
+pub fn deserialize_datetime<'de, D>(value: D) -> std::result::Result<DateTime<Utc>, D::Error> where D: Deserializer<'de> {
+    Ok(Utc.timestamp_millis(i64::deserialize(value)?))
+}
+
+pub fn deserialize_datetime_opt<'de, D>(value: D) -> std::result::Result<Option<DateTime<Utc>>, D::Error> where D: Deserializer<'de> {
+    if let Some(v) = Option::<i64>::deserialize(value)? {
+        Ok(Some(Utc.timestamp_millis(v)))
+    } else {
+        Ok(None)
+    }
+}
+
+pub fn deserialize_datetime_opt_opt<'de, D>(value: D) -> std::result::Result<Option<Option<DateTime<Utc>>>, D::Error> where D: Deserializer<'de> {
+    if let Some(v) = Option::<Option<i64>>::deserialize(value)? {
+        Ok(Some(v.map(|v| Utc.timestamp_millis(v))))
+    } else {
+        Ok(None)
+    }
+}
+
+// Date
+
+pub fn serialize_date<S>(value: &Date<Utc>, s: S) -> std::result::Result<S::Ok, S::Error> where S: Serializer {
+    s.serialize_i64(value.and_hms(0, 0, 0).timestamp())
+}
+
+pub fn deserialize_date<'de, D>(value: D) -> std::result::Result<Date<Utc>, D::Error> where D: Deserializer<'de> {
+    Ok(Utc.timestamp(i64::deserialize(value)?, 0).date())
 }
