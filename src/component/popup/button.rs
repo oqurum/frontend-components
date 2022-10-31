736213609ec5e057734f292a6f194648700642fc
Its <2779546+Its-its@users.noreply.github.com>
2022-10-30 22:30:03 -0700

    ButtonWithPopup: Utilize Popup At Point

--- src/component/popup/button.rs
+++ src/component/popup/button.rs
@@ -1,6 +1,9 @@
+use web_sys::HtmlElement;
 use yew::prelude::*;
 
 use super::{Popup, PopupType};
+
+// TODO: Update on Resize
 
 // TODO: Implement.
 // #[derive(Clone, Copy)]
@@ -25,7 +28,8 @@ pub enum ButtonMsg {
 }
 
 pub struct ButtonWithPopup {
-    is_open: bool,
+    open_pos: Option<(i32, i32)>,
+    but_ref: NodeRef,
 }
 
 impl Component for ButtonWithPopup {
@@ -33,17 +37,27 @@ impl Component for ButtonWithPopup {
     type Properties = ButtonProperty;
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self { is_open: false }
+        Self {
+            open_pos: None,
+            but_ref: NodeRef::default(),
+        }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             ButtonMsg::TogglePopup => {
-                self.is_open = !self.is_open;
+                if self.open_pos.is_none() {
+                    let element = self.but_ref.cast::<HtmlElement>().unwrap();
+                    let bb = element.get_bounding_client_rect();
+
+                    self.open_pos = Some(((bb.x() + bb.width()) as i32, bb.y() as i32));
+                } else {
+                    self.open_pos = None;
+                }
             }
 
             ButtonMsg::ClosePopup => {
-                self.is_open = false;
+                self.open_pos = None;
             }
         }
 
@@ -54,20 +68,23 @@ impl Component for ButtonWithPopup {
         html! {
             <div class={ classes!("button-popup-group", ctx.props().class.clone()) }>
                 <span
+                    ref={ self.but_ref.clone() }
                     class="button material-icons"
                     title="More Options"
                     onclick={ ctx.link().callback(|e: MouseEvent| {
                         e.prevent_default();
+                        e.stop_immediate_propagation();
+                        e.stop_propagation();
 
                         ButtonMsg::TogglePopup
                     }) }
                 >{ "more_horiz" }</span>
 
                 {
-                    if self.is_open {
+                    if let Some((x, y)) = self.open_pos {
                         html! {
                             <Popup
-                                type_of={ PopupType::Display }
+                                type_of={ PopupType::AtPoint(x, y) }
                                 on_close={ ctx.link().callback(|_| ButtonMsg::ClosePopup) }
                                 classes="menu-list"
                             >
